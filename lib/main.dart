f77a5b8e7e2ecfb21e160679fd4ebeb7884610f4
Arshdeep2001 <johndoe@example.com>
2020-10-12 22:19:06 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:chess_game/main.dart';
+import 'package:flutter_chess_board/flutter_chess_board.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      home: Home(),
+    );
+  }
+}
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            ChessBoard(
+              boardType: BoardType.orange,
+              onMove: (move) {
+                print(move);
+              },
+              onCheckMate: (color) {
+                print(color);
+              },
+              onDraw: () {},
+              size: MediaQuery.of(context).size.width,
+              enableUserMoves: true,
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
